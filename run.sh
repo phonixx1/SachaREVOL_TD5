087f8944dccb3bee6a5aa9747898e18ffdba1490
sacha <revol.1998@outlook.fr>
2021-03-13 16:38:01 +0100

    exe mode on py file + add of the run.sh file

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Check is venc package is install
+if [ "$(pip3 freeze | grep virtualenv)" ]
+then
+	echo Package venv well installed
+	echo
+else
+	echo First install virtualenv -> "sudo pip3 install virtualenv "
+	exit 1
+fi
+
+# Check if venv file is create
+fileEnv=.env
+if [ -d "$fileEnv" ]
+then
+	echo Env already create
+	echo
+else
+	virtualenv "$fileEnv"
+fi
+
+source "$fileEnv/bin/activate"
+
+pip3 install -r requirements.txt
+
+echo Dependencies well load
+echo
+echo Begining of .py script
+echo
+chmod +x mainFile.py
+
+./mainFile.py
+
+echo End
